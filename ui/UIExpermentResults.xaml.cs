e23ae03a50de7bf3e13686fe05134f63bc7c3542
Ammar Hawbani <33313489+howbani@users.noreply.github.com>
2020-03-19 12:16:17 +0800

    Add files via upload

---
+++ ui/UIExpermentResults.xaml.cs
@@ -0,0 +1,24 @@
+﻿using TreeBased.Charts;
+using TreeBased.Intilization;
+using TreeBased.ExpermentsResults;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace TreeBased.ui
+{
+    /// <summary>
+    /// Interaction logic for ExpermentResults.xaml
+    /// </summary>
+    public partial class UIExpermentResults : Window 
+    {
+        public UIExpermentResults() 
+        {
+            InitializeComponent();
+            /*
+            List<CompareResult> list = new List<CompareResult>();
+            CompareResult res = CompareResultsClass.GetEnergyDelayForAnExperment();
+            list.Add(res);
+            dg_data.ItemsSource = list;*/
+        }
+    }
+}
